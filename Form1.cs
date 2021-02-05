4c692794a0fa36226a0c41f23ca1354e240e9ccc
Matt Peterson <matt.peterson@roehl.net>
2021-02-05 11:54:35 -0600

    first

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WebView2Tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var frm = await Form2.CreateFormAsync();
+            frm.Navigate("http://google.com");
+
+            // lol wut. Without this the form is displayed as a blank.
+            //await Task.Delay(1);
+
+            // shows the form with the WebView2.
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebView2Tester
+{
+    public partial class Form2 : Form
+    {
+        private Form2()
+        {
+            InitializeComponent();
+            this.FormClosed += Form2_FormClosed;
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Need to handle disposing the WebView2 on ShowDialog, otherwise the form closes but the browser pane seems to remain.
+            // .Visible = false required, otherwise .Dispose throws an exception.
+            webView.Visible = false;
+            webView.Dispose();
+        }
+
+        public static async Task<Form2> CreateFormAsync()
+        {
+            var browser = new Form2();
+            await browser.InitializeAsync();
+
+            return browser;
+        }
+
+        Task InitializeAsync()
+        {
+            return webView.EnsureCoreWebView2Async(null);
+        }
+
+        public void Navigate(string url)
+        {
+            webView.CoreWebView2.Navigate(url);
+        }
+    }
+}
